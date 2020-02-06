1ac4a14d79c400806b1de5d56b23859e9c82fac8
LeJohne <johngoy11024@gmail.com>
2020-02-06 10:32:02 -0500

    commit 3 - fix initial setup

--- js/app.js
+++ js/app.js
@@ -1,9 +1,79 @@
-///////////////////// CONSTANTS /////////////////////////////////////
+const winningConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
 
 ///////////////////// APP STATE (VARIABLES) /////////////////////////
 
+let board;
+let turn;
+let win;
+
 ///////////////////// CACHED ELEMENT REFERENCES /////////////////////
+
+const squares = Array.from(document.querySelectorAll("#board div"));
+const message = document.querySelector("h2");
 
 ///////////////////// EVENT LISTENERS ///////////////////////////////
 
+window.onload = init;
+
+document.getElementById("board").onclick = takeTurn;
+document.getElementById("reset-button").onclick = init;
+
 ///////////////////// FUNCTIONS /////////////////////////////////////
+
+function init() {
+  board = ["", "", "", "", "", "", "", "", ""];
+  turn = "X";
+  win = null;
+
+  render();
+}
+
+function render() {
+  board.forEach(function(mark, index) {
+    squares[index].textContent = mark;
+  });
+
+  message.textContent =
+    win === "T" ? "It's a tie!" : win ? `${win} wins!` : `Turn: ${turn}`;
+}
+
+function takeTurn(e) {
+  if (!win) {
+    let index = squares.findIndex(function(square) {
+      return square === e.target;
+    });
+
+    if (board[index] === "") {
+      board[index] = turn;
+      turn = turn === "X" ? "O" : "X";
+      win = getWinner();
+
+      render();
+    }
+  }
+}
+
+function getWinner() {
+  let winner = null;
+
+  winningConditions.forEach(function(condition, index) {
+    if (
+      board[condition[0]] &&
+      board[condition[0]] === board[condition[1]] &&
+      board[condition[1]] === board[condition[2]]
+    ) {
+      winner = board[condition[0]];
+    }
+  });
+
+  return winner ? winner : board.includes("") ? null : "T";
+}
