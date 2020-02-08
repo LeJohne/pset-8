488230e9087dda4dec6a3623b6eaddf0b8c63e81
LeJohne <johngoy11024@gmail.com>
2020-02-08 18:21:26 -0500

    commit 9 - organized js

--- js/app.js
+++ js/app.js
@@ -1,93 +1,4 @@
-board// const winningConditions = [
-//   [0, 1, 2],
-//   [3, 4, 5],
-//   [6, 7, 8],
-//   [0, 3, 6],
-//   [1, 4, 7],
-//   [2, 5, 8],
-//   [0, 4, 8],
-//   [2, 4, 6]
-// ];
-// const aiPlayer = "X"
-// const huPlayer = "O"
-//
-// ///////////////////// APP STATE (VARIABLES) /////////////////////////
-//
-// let board;
-// let turn;
-// let win;
-//
-// ///////////////////// CACHED ELEMENT REFERENCES /////////////////////
-//
-// const squares = Array.from(document.querySelectorAll("#board div"));
-// const message = document.querySelector("h2");
-// const victoryAudio = document.getElementById("victory");
-//
-// ///////////////////// EVENT LISTENERS ///////////////////////////////
-//
-// window.onload = init;
-//
-// document.getElementById("board").onclick = takeTurn;
-// document.getElementById("reset-button").onclick = init;
-//
-// ///////////////////// FUNCTIONS /////////////////////////////////////
-//
-// function init() {
-//   board = ["", "", "", "", "", "", "", "", ""];
-//   turn = "X";
-//   win = null;
-//
-//   render();
-//   victoryAudio.pause();
-//   victoryAudio.currentTime = 0;
-// }
-//
-// function render() {
-//   board.forEach(function(mark, index) {
-//     squares[index].textContent = mark;
-//   });
-//
-//   message.textContent =
-//     win === "T" ? "It's a tie!" : win ? `${win} wins!` : `Turn: ${turn}`;
-// }
-//
-// function takeTurn(e) {
-//   if (!win) {
-//     let index = squares.findIndex(function(square) {
-//       return square === e.target;
-//     });
-//
-//     if (board[index] === "") {
-//        board[index] = turn;
-//        turn = turn === "X" ? "O" : "X";
-//        win = getWinner();
-//        if (win === "X") {
-//            victoryAudio.play();
-//        } else if (win === "O") {
-//            victoryAudio.play();
-//        }
-//
-//       render();
-//     }
-//   }
-// }
-//
-// function getWinner() {
-//   let winner = null;
-//
-//   winningConditions.forEach(function(condition, index) {
-//     if (
-//       board[condition[0]] &&
-//       board[condition[0]] === board[condition[1]] &&
-//       board[condition[1]] === board[condition[2]]
-//     ) {
-//       winner = board[condition[0]];
-//     }
-//   });
-//   return winner ? winner : board.includes("") ? null : "T";
-// }
-
-
+///////////////////// CONSTANTS /////////////////////////////////////
 
 var origBoard;
 const huPlayer = 'O';
@@ -103,9 +14,12 @@ const winCombos = [
 	[6, 4, 2]
 ]
 
+///////////////////// CACHED ELEMENT REFERENCES /////////////////////
+
 const cells = document.querySelectorAll('.cell');
 startGame();
 
+///////////////////// FUNCTIONS /////////////////////////////////////
 function startGame() {
 	document.querySelector(".endgame").style.display = "none";
 	origBoard = Array.from(Array(9).keys());
