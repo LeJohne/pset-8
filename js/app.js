a54062f967b46db5b9cb3d91783f880fbfefb45f
LeJohne <johngoy11024@gmail.com>
2020-02-08 16:46:26 -0500

    commit 8 - added AI

--- js/app.js
+++ js/app.js
@@ -1,86 +1,231 @@
-const winningConditions = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
-];
+board// const winningConditions = [
+//   [0, 1, 2],
+//   [3, 4, 5],
+//   [6, 7, 8],
+//   [0, 3, 6],
+//   [1, 4, 7],
+//   [2, 5, 8],
+//   [0, 4, 8],
+//   [2, 4, 6]
+// ];
+// const aiPlayer = "X"
+// const huPlayer = "O"
+//
+// ///////////////////// APP STATE (VARIABLES) /////////////////////////
+//
+// let board;
+// let turn;
+// let win;
+//
+// ///////////////////// CACHED ELEMENT REFERENCES /////////////////////
+//
+// const squares = Array.from(document.querySelectorAll("#board div"));
+// const message = document.querySelector("h2");
+// const victoryAudio = document.getElementById("victory");
+//
+// ///////////////////// EVENT LISTENERS ///////////////////////////////
+//
+// window.onload = init;
+//
+// document.getElementById("board").onclick = takeTurn;
+// document.getElementById("reset-button").onclick = init;
+//
+// ///////////////////// FUNCTIONS /////////////////////////////////////
+//
+// function init() {
+//   board = ["", "", "", "", "", "", "", "", ""];
+//   turn = "X";
+//   win = null;
+//
+//   render();
+//   victoryAudio.pause();
+//   victoryAudio.currentTime = 0;
+// }
+//
+// function render() {
+//   board.forEach(function(mark, index) {
+//     squares[index].textContent = mark;
+//   });
+//
+//   message.textContent =
+//     win === "T" ? "It's a tie!" : win ? `${win} wins!` : `Turn: ${turn}`;
+// }
+//
+// function takeTurn(e) {
+//   if (!win) {
+//     let index = squares.findIndex(function(square) {
+//       return square === e.target;
+//     });
+//
+//     if (board[index] === "") {
+//        board[index] = turn;
+//        turn = turn === "X" ? "O" : "X";
+//        win = getWinner();
+//        if (win === "X") {
+//            victoryAudio.play();
+//        } else if (win === "O") {
+//            victoryAudio.play();
+//        }
+//
+//       render();
+//     }
+//   }
+// }
+//
+// function getWinner() {
+//   let winner = null;
+//
+//   winningConditions.forEach(function(condition, index) {
+//     if (
+//       board[condition[0]] &&
+//       board[condition[0]] === board[condition[1]] &&
+//       board[condition[1]] === board[condition[2]]
+//     ) {
+//       winner = board[condition[0]];
+//     }
+//   });
+//   return winner ? winner : board.includes("") ? null : "T";
+// }
 
-///////////////////// APP STATE (VARIABLES) /////////////////////////
 
-let board;
-let turn;
-let win;
 
-///////////////////// CACHED ELEMENT REFERENCES /////////////////////
+var origBoard;
+const huPlayer = 'O';
+const aiPlayer = 'X';
+const winCombos = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[6, 4, 2]
+]
 
-const squares = Array.from(document.querySelectorAll("#board div"));
-const message = document.querySelector("h2");
-const victoryAudio = document.getElementById("victory");
+const cells = document.querySelectorAll('.cell');
+startGame();
 
-///////////////////// EVENT LISTENERS ///////////////////////////////
-
-window.onload = init;
-
-document.getElementById("board").onclick = takeTurn;
-document.getElementById("reset-button").onclick = init;
-
-///////////////////// FUNCTIONS /////////////////////////////////////
-
-function init() {
-  board = ["", "", "", "", "", "", "", "", ""];
-  turn = "X";
-  win = null;
-
-  render();
-  victoryAudio.pause();
-  victoryAudio.currentTime = 0;
+function startGame() {
+	document.querySelector(".endgame").style.display = "none";
+	origBoard = Array.from(Array(9).keys());
+	for (var i = 0; i < cells.length; i++) {
+		cells[i].innerText = '';
+		cells[i].style.removeProperty('background-color');
+		cells[i].addEventListener('click', turnClick, false);
+	}
 }
 
-function render() {
-  board.forEach(function(mark, index) {
-    squares[index].textContent = mark;
-  });
-
-  message.textContent =
-    win === "T" ? "It's a tie!" : win ? `${win} wins!` : `Turn: ${turn}`;
+function turnClick(square) {
+	if (typeof origBoard[square.target.id] == 'number') {
+		turn(square.target.id, huPlayer)
+		if (!checkWin(origBoard, huPlayer) && !checkTie()) turn(bestSpot(), aiPlayer);
+	}
 }
 
-function takeTurn(e) {
-  if (!win) {
-    let index = squares.findIndex(function(square) {
-      return square === e.target;
-    });
-
-    if (board[index] === "") {
-       board[index] = turn;
-       turn = turn === "X" ? "O" : "X";
-       win = getWinner();
-       if (win === "X") {
-           victoryAudio.play();
-       } else if (win === "O") {
-           victoryAudio.play();
-       }
-
-      render();
-    }
-  }
+function turn(squareId, player) {
+	origBoard[squareId] = player;
+	document.getElementById(squareId).innerText = player;
+	let gameWon = checkWin(origBoard, player)
+	if (gameWon) gameOver(gameWon)
 }
 
-function getWinner() {
-  let winner = null;
+function checkWin(board, player) {
+	let plays = board.reduce((a, e, i) =>
+		(e === player) ? a.concat(i) : a, []);
+	let gameWon = null;
+	for (let [index, win] of winCombos.entries()) {
+		if (win.every(elem => plays.indexOf(elem) > -1)) {
+			gameWon = {index: index, player: player};
+			break;
+		}
+	}
+	return gameWon;
+}
 
-  winningConditions.forEach(function(condition, index) {
-    if (
-      board[condition[0]] &&
-      board[condition[0]] === board[condition[1]] &&
-      board[condition[1]] === board[condition[2]]
-    ) {
-      winner = board[condition[0]];
-    }
-  });
-  return winner ? winner : board.includes("") ? null : "T";
+function gameOver(gameWon) {
+	for (let index of winCombos[gameWon.index]) {
+		document.getElementById(index).style.backgroundColor =
+			gameWon.player == huPlayer ? "blue" : "rgb(220,20,60,0.35)";
+	}
+	for (var i = 0; i < cells.length; i++) {
+		cells[i].removeEventListener('click', turnClick, false);
+	}
+	declareWinner(gameWon.player == huPlayer ? "You win!" : "You lose.");
+}
+
+function declareWinner(who) {
+	document.querySelector(".endgame").style.display = "block";
+	document.querySelector(".endgame .text").innerText = who;
+}
+
+function emptySquares() {
+	return origBoard.filter(s => typeof s == 'number');
+}
+
+function bestSpot() {
+	return minimax(origBoard, aiPlayer).index;
+}
+
+function checkTie() {
+	if (emptySquares().length == 0) {
+		for (var i = 0; i < cells.length; i++) {
+			cells[i].style.backgroundColor = "rgb(50,205,50,0.35)";
+			cells[i].removeEventListener('click', turnClick, false);
+		}
+		declareWinner("Tie Game!")
+		return true;
+	}
+	return false;
+}
+
+function minimax(newBoard, player) {
+	var availSpots = emptySquares();
+
+	if (checkWin(newBoard, huPlayer)) {
+		return {score: -10};
+	} else if (checkWin(newBoard, aiPlayer)) {
+		return {score: 10};
+	} else if (availSpots.length === 0) {
+		return {score: 0};
+	}
+	var moves = [];
+	for (var i = 0; i < availSpots.length; i++) {
+		var move = {};
+		move.index = newBoard[availSpots[i]];
+		newBoard[availSpots[i]] = player;
+
+		if (player == aiPlayer) {
+			var result = minimax(newBoard, huPlayer);
+			move.score = result.score;
+		} else {
+			var result = minimax(newBoard, aiPlayer);
+			move.score = result.score;
+		}
+
+		newBoard[availSpots[i]] = move.index;
+
+		moves.push(move);
+	}
+
+	var bestMove;
+	if(player === aiPlayer) {
+		var bestScore = -10000;
+		for(var i = 0; i < moves.length; i++) {
+			if (moves[i].score > bestScore) {
+				bestScore = moves[i].score;
+				bestMove = i;
+			}
+		}
+	} else {
+		var bestScore = 10000;
+		for(var i = 0; i < moves.length; i++) {
+			if (moves[i].score < bestScore) {
+				bestScore = moves[i].score;
+				bestMove = i;
+			}
+		}
+	}
+
+	return moves[bestMove];
 }
